576affbd41e1fd734b8a2a92da134e3b91fee764
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-03-25 14:43:57 +0300

    form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,10 @@ import {Quotes} from './quotes'
 })
 export class AppComponent {
     quotes = [
-      new Quotes(1,'The grass is greener where you water it','When you want to make your life feel better and your thoughts more positive'),
-      new Quotes(2,'When you cannot find the sunshine, be the sunshine','When you’re feeling blue or things are a little flat'),
-      new Quotes(3, 'Little by little, day by day, what is meant for you will find its way','When good things are taking too long to manifest and you’re starting to lose faith'),
-      new Quotes(4, 'Learn from yesterday, live for today, hope for tomorrow','When you feel like yesterday was a struggle and you want to create better tomorrows'),
-      new Quotes(5, 'Believe, act as if, live, like you already have it.','When you need a few words of encouragement, support and inspiration'),
-
+      new Quotes(1,'The grass is greener where you water it','When you want to make your life feel better and your thoughts more positive',new Date(2018,3,4),'John Doe' ),
+      new Quotes(2,'When you cannot find the sunshine, be the sunshine','When you’re feeling blue or things are a little flat',new Date(2018,3,4),'Anonymous' ),
+      new Quotes(3, 'Little by little, day by day, what is meant for you will find its way','When good things are taking too long to manifest and you’re starting to lose faith',new Date(2018,3,4), 'John Doe'),
+      new Quotes(4, 'Learn from yesterday, live for today, hope for tomorrow','When you feel like yesterday was a struggle and you want to create better tomorrows',new Date(2018,3,4),'Anonymous'  ),
+      new Quotes(5, 'Believe, act as if, live, like you already have it.','When you need a few words of encouragement, support and inspiration',new Date(2018,3,4),'John Doe'),
     ]
 }
--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,6 +1,6 @@
 export class Quotes {
   public showDescription:boolean;
-   constructor(public id:number, public name:string, public description:string){
+   constructor(public id:number, public name:string, public description:string,public submitDate:Date,  public author:string){
      this.showDescription=false
 
    }
